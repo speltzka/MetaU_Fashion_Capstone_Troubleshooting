c5f9069b965383f5fc939124022a7e299fb6c2c2
Precious Onah <precious20@meta.com>
2023-07-06 18:56:41 -0700

    Fetch the API that contains fashion images

--- fashion-messaging-app-ui/src/components/App/App.jsx
+++ fashion-messaging-app-ui/src/components/App/App.jsx
@@ -1,13 +1,35 @@
-import './App.css'
+import "./App.css"
 import Search from "../Search/Search";
 import Navbar from "../Navbar/Navbar";
+import { useEffect, useState } from "react";
+import { createClient } from "pexels";
 
-export default function App() {
-  return (   
-    <div className="container">
-      <h1>Aesthetik.</h1>
-        <Search />
-        <Navbar />
-  </div>
-  )
+const client = createClient("06ZcwjgbmJBM8T2TxLUZ5iwdXXGxiAgz0Z018b7QPKwR1ExipkFjaAuw");
+const query = "Fashion";
+const numberOfPhotos = 100;
+
+function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    client.photos.search({ query, per_page: numberOfPhotos })
+      .then(response => {
+        setPhotos(response.photos);
+      })
+      .catch(error => {
+        console.error("Error fetching photos:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+       <h1>Aesthetik.</h1>
+       <Search />
+       <Navbar />
+      </div>       
+    </div> 
+  );
 }
+
+export default App;
