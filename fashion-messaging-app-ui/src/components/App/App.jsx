399aa6223f103ea961f179c496500c5fcf3c3aaa
Precious Onah <precious20@meta.com>
2023-07-06 18:56:41 -0700

    correct variable name for constant and change API variable name

--- fashion-messaging-app-ui/src/components/App/App.jsx
+++ fashion-messaging-app-ui/src/components/App/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
+import "./App.css"
 import Search from "../Search/Search";
 import Navbar from "../Navbar/Navbar";
+import { useEffect, useState } from "react";
+import { createClient } from "pexels";
+
+const clientAPI = createClient("06ZcwjgbmJBM8T2TxLUZ5iwdXXGxiAgz0Z018b7QPKwR1ExipkFjaAuw");
+const query = "Fashion";
+const PHOTOS = 100;
 
 export default function App() {
-  return (   
-    <div className="container">
-      <h1>Aesthetik.</h1>
-        <Search />
-        <Navbar />
-  </div>
-  )
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    clientAPI.photos.search({ query, per_page: PHOTOS })
+      .then(response => {
+        setPhotos(response.photos);
+      })
+      .catch(error => {
+        console.error("Error fetching photos:", error);
+      });
+  }, []);
+  return (
+    <div>
+      <div className="container">
+       <h1>Aesthetik.</h1>
+       <Search />
+       <Navbar />
+      </div>       
+    </div> 
+  );
 }
