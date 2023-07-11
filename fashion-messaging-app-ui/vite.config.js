a1ca34d87c9d2a0fd5b1bd91c527cee2dbd7945f
speltzka <speltzka@yahoo.com>
2023-07-11 14:00:12 -0700

    Update vite.config.js
    Adding a comma

--- fashion-messaging-app-ui/vite.config.js
+++ fashion-messaging-app-ui/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MetaU_Fashion_Capstone_Troubleshooting/'
+  base: '/MetaU_Fashion_Capstone_Troubleshooting/',
   plugins: [react()],
 })
