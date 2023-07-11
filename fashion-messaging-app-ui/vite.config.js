e1949c821e3c55108a7b5ed24f648fc44d47a853
speltzka <speltzka@yahoo.com>
2023-07-11 14:35:53 -0700

    Update vite.config.js
    Resetting base name

--- fashion-messaging-app-ui/vite.config.js
+++ fashion-messaging-app-ui/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MetaU_Fashion_Capstone_Troubleshooting/fashion-messaging-app-ui/',
+  base: '/MetaU_Fashion_Capstone_Troubleshooting/',
   plugins: [react()],
 })
